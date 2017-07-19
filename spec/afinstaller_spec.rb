782456ca314e738abb43e0034346cdaf04fdaad0
Gabe Morales <gabomorales@me.com>
2017-07-19 13:41:54 -0500

    Updated tests

--- spec/afinstaller_spec.rb
+++ spec/afinstaller_spec.rb
@@ -1,16 +1,6 @@
-# require "spec_helper"
-#
-# RSpec.describe Afinstaller do
-#   it "has a version number" do
-#     expect(Afinstaller::VERSION).not_to be nil
-#   end
-# end
-
 require 'afinstaller'
 describe Afinstaller::Installers::IOS do
   it "ios is ios" do
-    # expect(Afinstaller::Installer.iOS("iOS")).to eql("iOS!")
-    # Afinstaller::Installers::IOS.start([version])
-    # expect(Afinstaller::Installers::IOS.iOS("4.5.0")).to eql("Correct!")
+    # Add tests here
   end
 end
