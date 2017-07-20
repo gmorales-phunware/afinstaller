b50dbcaaf768e6fa22bcc1df4670043d4ce48378
Gabe Morales <gabomorales@me.com>
2017-07-19 20:53:59 -0500

    Added support for Android installer

--- lib/afinstaller/cli.rb
+++ lib/afinstaller/cli.rb
@@ -5,21 +5,54 @@ require 'afinstaller/installers/iOS/iOSBuild'
 require 'afinstaller/installers/Android/android'
 require 'afinstaller/installers/Android/androidBuild'
 
+def system!(*args)
+  system(*args) || abort(''+red('\n== Command #{args} failed =="')+'')
+end
+
 module Afinstaller
   class CLI < Thor
-    desc "portray ITEM", "Determines if a piece of food is gross or delicious"
+    check_unknown_options!
+    def self.exit_on_failure?
+    true
+    end
     method_option :build, :aliases => "-b", :desc => "Build project after cloning repo."
+
+    desc "iOS [version]", "CLI Installer for AF iOS "
+    long_desc <<-LONGDESC
+    `afinstaller iOS [version]` will clone the AF iOS repo with the version you specify. It will then run `pod install`,
+    it will finally open the project folder.
+
+    You can optionally specify the build flag [-b], which will build your project after cocoapod installation
+
+    > $ afinstaller iOS 4.5.0 -b
+  LONGDESC
     def iOS(version)
       build_project = options[:build]
       if build_project
-        puts "Building with -b flag"
         Afinstaller::Installers::IOSBuild.start([version])
       else
-        puts "Building without -b flag"
         Afinstaller::Installers::IOS.start([version])
       end
     end
 
+    desc "Android [version]", "CLI Installer for AF Android "
+    long_desc <<-LONGDESC
+    `afinstaller Android [version]` will clone the AF Android repo with the version you specify. It will it will
+    finally open the project folder.
+
+    You can optionally specify the build flag [-b], which will attempt to run Gradle Clean.
+    This requires Java 8.0 and above.
+
+    > $ afinstaller Android 4.5.0 -b
+  LONGDESC
+    def Android(version)
+      build_project = options[:build]
+      if build_project
+        Afinstaller::Installers::AndroidBuild.start([version])
+      else
+        Afinstaller::Installers::Android.start([version])
+      end
+    end
 
   end
 end
